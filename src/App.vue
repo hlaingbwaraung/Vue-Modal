d81a49aa1399e4cf32000cd40b874a77c0969b3b
hbanihon <mizbwar@gmail.com>
2022-03-25 02:32:56 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>hi </h1>
+  <div v-if="showModal">
+      <Modal :theme="theme" @close="showModal=false">
+        <h1>Success log in</h1>
+        <p>welcome user</p>
+        </Modal>
+  </div>
+   <div v-if="dangerModal">
+      <Modal :theme="theme" @close="dangerModal=false">
+        <h1>Are u sure to delete</h1>
+        <p>Its hard to say goodbye</p>
+        </Modal>
+  </div>
+  <button @click="showModal=true">open modal</button>
+  <button @click="dangerModal=true">Delete modal</button>
+
+</template>
+
+<script>
+import Modal from "./components/Modal.vue"
+export default {
+  data(){
+    return{   
+       theme:"danger",
+       showModal:false,    
+    }
+  },
+  components:{
+    Modal
+  }
+}
+</script>
+
+<style scoped>
+ h1{
+  color: aqua;
+}
+</style>
+
---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+      <div class="modal" :class="{success:theme==='success',danger:theme==='danger'}">
+         <slot/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props:['theme'],
+ methods:{
+     closeModal(){
+         this.$emit("close")
+     }
+}
+}
+</script>
+
+<style>
+    .backdrop{
+        position: fixed;
+        top: 0;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height:100%;
+    }
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: white;
+        border-radius: 10px;
+    }
+    .success{
+        background-color: green;
+        color:white;
+    }
+    .danger{
+         background-color: crimson;
+        color:white;
+    }
+</style>
